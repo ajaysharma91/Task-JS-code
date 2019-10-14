80aed7791074f67773454f92eb30a0849078cde1
ajaysharma91 <ajparashar111@gmail.com>
2019-10-14 15:22:07 +0530

    there are so many importantt file addded

--- toDo/js/todo.js
+++ toDo/js/todo.js
@@ -27,9 +27,12 @@ $(document).ready(function(){
      {
      	//alert("call add function");
      	var li = document.createElement("li");
-     	var text = document.getElementById("text1");
+     	var text = document.getElementById("text1").value;
      	var txt = document.createTextNode(text);
+     	//var txt = document.createTextNode(text);
+     	
      	li.appendChild(txt);
+
      	li.className="list-group-item";
      	if(text ==="")
      	{
@@ -45,16 +48,22 @@ $(document).ready(function(){
     
      	var span = document.createElement("SPAN");
      	var txt = document.createTextNode("\u00D7");
+     	var span1 = document.createElement("SPAN");
+     	var edit = document.createTextNode("Edit");
      	span.className="close";
+     	//span1.className="glyphicon glyphicon-pencil";
      	span.appendChild(txt);
+     	//span1.appendChild(edit);
      	span.style.color = "#ff0000";
      	//alert(myList[i].value);
      	myList[myList.length-1].appendChild(span);
+     	//myList[myList.length-1].appendChild(span1);
    
 
 
      var c = document.getElementsByClassName("close");
      for (var i = 0; i < c.length; i++) {
+     	 //alert(c[0]);
      	 c[i].onclick = function(){
      	 	var div = this.parentElement;
      	 	div.style.display="none";
